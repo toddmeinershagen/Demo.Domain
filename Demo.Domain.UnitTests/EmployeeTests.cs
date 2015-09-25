0f3eea5c08d163d84ef87185a950b0cf65bab3fb
Todd Meinershagen <todd@meinershagen.net>
2015-09-25 18:24:03 -0500

    Initial commit

---
+++ Demo.Domain.UnitTests/EmployeeTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+
+namespace Demo.Domain.UnitTests
+{
+    [TestFixture]
+    public class EmployeeTests
+    {
+        [Test]
+        public void TestFailure()
+        {
+            var employee = new Employee();
+            Assert.That(employee.IsValid(), Is.False);
+        }
+
+        [Test]
+        public void TestSuccess()
+        {
+            var employee = new Employee{FirstName = "Todd"};
+            Assert.That(employee.IsValid(), Is.True);
+        }
+
+        [Test]
+        public void TestCustomCheck()
+        {
+            var employee = new Employee
+            {
+                FirstName = "ToddMeinershagenToddMeinershagenToddMeinershagen",
+                LastName = "ToddMeinershagenToddMeinershagenToddMeinershagen"
+            };
+
+            Assert.That(employee.IsValid(), Is.False);
+            Assert.That(employee.Validate()[0].ErrorMessage, Is.EqualTo("The full name must not be longer than 100 characters."));
+        }
+    }
+}
---
+++ Demo.Domain/Employee.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Demo.Domain
+{
+    [CustomValidation(typeof(Employee), "CustomCheck")]
+    public class Employee
+    {
+        [Required(ErrorMessage = "First Name is a required property.")]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public static ValidationResult CustomCheck(Employee employee, ValidationContext context)
+        {
+            var fullName = String.Format("{0} {1}", employee.FirstName, employee.LastName);
+            return fullName.Length > 50 
+                ? new ValidationResult("The full name must not be longer than 100 characters.") 
+                : ValidationResult.Success;
+        }
+
+        /// <summary>
+        /// Determines whether this instance is valid.
+        /// </summary>
+        /// <remarks>
+        /// This is a simple validation method that just returns a true/false without any error details.
+        /// </remarks>
+        /// <remarks>
+        /// </remarks>
+        /// <returns></returns>
+        public bool IsValid()
+        {
+            var context = new ValidationContext(this, null, null);
+            var results = new List<ValidationResult>();
+            Validator.TryValidateObject(this, context, results);
+
+            return results.Count == 0;
+        }
+
+        /// <summary>
+        /// Validates this instance.
+        /// </summary>
+        /// <remarks>
+        /// This is the more comprehensive version of validation that returns a list of the error messages.
+        /// </remarks>
+        /// <returns></returns>
+        public List<ValidationResult> Validate()
+        {
+            var context = new ValidationContext(this, null, null);
+            var results = new List<ValidationResult>();
+            Validator.TryValidateObject(this, context, results);
+
+            return results;
+        }
+    }
+}
